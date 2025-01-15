e61b4eb3649b67cc273593ea1f2a1f23e5b77908
vamshi <vamshireddy19421@gmail.com>
2025-01-15 18:43:57 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Game from './Game'
-import Footer from './Footer'
+
 
 const App = () => {
   return (
     <div>
       <Game/>
-      <Footer/>
     </div>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -47,93 +47,111 @@ export default function Game() {
     Game_Logic();
     if (userChoice === "PAPER") {
       setChoiceImage(
-        <div className="border-[10px] border-blue-500 bg-white p-5 h-[100px] w-[100px] rounded-[50%]">
-          <img className=" object-cover" src={paper} alt="PAPER" />
+        <div className="flex justify-center items-center bg-white border-[20px] border-blue-600 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200">
+          <img className=" object-cover " src={paper} alt="PAPER" />
         </div>
       );
     } else if (userChoice === "ROCK") {
       setChoiceImage(
-        <div className="border-[10px] border-red-500 bg-white p-5 h-[100px] w-[100px] rounded-[50%]">
-          <img className=" object-cover" src={rock} alt="ROCK" />
+        <div className="flex justify-center items-center bg-white border-[20px] border-red-600 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200">
+          <img className=" object-cover " src={rock} alt="ROCK" />
         </div>
       );
     } else if (userChoice === "SCISSORS") {
       setChoiceImage(
-        <div className="border-[10px] border-yellow-500 bg-white p-5 h-[100px] w-[100px] rounded-[50%]">
-          <img className=" object-cover" src={scissors} alt="SCISSORS" />
+        <div className="flex justify-center items-center bg-white border-[20px] border-yellow-600 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200">
+          <img className=" object-cover " src={scissors} alt="SCISSORS" />
         </div>
       );
     }
   }, [userChoice]);
 
   return (
-    <main className=" flex flex-col items-center mt-10">
-      <div className=" flex flex-row w-auto border border-gray-200 rounded-[15px] justify-between px-4 py-3 md:w-[50rem]">
-        <div className=" self-center flex flex-col text-3xl font-bold text-white uppercase">
+    <main className="flex flex-col items-center mt-10 relative">
+      {/* Scoreboard Section */}
+      <div className="flex flex-row w-auto border border-gray-200 rounded-[15px] justify-between px-4 py-3 md:w-[50rem]">
+        <div className="self-center flex flex-col text-3xl font-bold text-white uppercase">
           <span>Rock</span>
           <span>Paper</span>
           <span>Scissors</span>
         </div>
 
-        <div className=" border border-transparent bg-white px-10 py-2 rounded-[5px] flex flex-col items-center gap-2">
+        <div className="border border-transparent bg-white px-10 py-2 rounded-[5px] flex flex-col items-center gap-2">
           <p className="text-blue-500 text-2xl font-bold">Score</p>
-          <p className=" text-5xl font-extrabold">{score}</p>
+          <p className="text-5xl font-extrabold">{score}</p>
         </div>
       </div>
 
-      <div className=" mt-10 flex flex-col items-center">
+      {/* Game Section */}
+      <div className="mt-10 flex flex-col items-center">
         {userChoice === null ? (
           <div>
-            <h1 className="text-sky-600 text-3xl font-bold">
-              Choose among rock, paper, scissors
-            </h1>
-            <div className=" mt-10 flex flex-col items-center">
-              <div className=" flex flex-wrap gap-10">
+            <div className="mt-20 flex flex-col items-center">
+              <div className="flex flex-wrap gap-10">
                 <button
                   onClick={() => setUserChoice("PAPER")}
-                  className=" flex justify-center items-center bg-white border-[10px] border-blue-500 h-[120px] w-[120px] rounded-[50%] hover:translate-y-[-5px] duration-200"
+                  className="flex justify-center items-center bg-white border-[10px] border-blue-500 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200"
                 >
-                  <img src={paper} alt="" />
+                  <img src={paper} alt="PAPER" />
                 </button>
 
                 <button
                   onClick={() => setUserChoice("SCISSORS")}
-                  className="flex justify-center items-center bg-white border-[10px] border-yellow-500 h-[120px] w-[120px] rounded-[50%] hover:translate-y-[-5px] duration-200"
+                  className="flex justify-center items-center bg-white border-[10px] border-yellow-500 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200"
                 >
-                  <img src={scissors} alt="" />
+                  <img src={scissors} alt="SCISSORS" />
                 </button>
               </div>
 
               <button
                 onClick={() => setUserChoice("ROCK")}
-                className="flex justify-center items-center bg-white border-[10px] border-red-500 h-[120px] w-[120px] rounded-[50%] hover:translate-y-[-5px] duration-200"
+                className="flex justify-center items-center bg-white border-[10px] border-red-500 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200 mt-5"
               >
-                <img src={rock} alt="" />
+                <img src={rock} alt="ROCK" />
               </button>
             </div>
           </div>
         ) : (
-          <div className=" flex flex-col gap-5 items-center">
-            <div className=" flex flex-row justify-between md:w-[50rem]">
-              <div>{choiceImage}</div>
-              <div className="border-[10px] border-orange-700 bg-white p-5 h-[100px] w-[100px] rounded-[50%]">
-                {opponentChoice}
+          <div className="flex flex-col items-center gap-8">
+            <div className="flex flex-row justify-center items-center gap-10 md:w-[40rem]">
+              {/* User's Choice */}
+              <div className="flex flex-col items-center  gap-3">
+                <h2 className="text-white mt-[50px] mb-[30px] font-bold">YOU PICKED</h2>
+               {choiceImage}
+              </div>
+
+              {/* Message and Play Again Button in the Center */}
+              <div className="flex flex-col items-center gap-5 mt-[90px] font-bold">
+                <p className="text-white text-3xl font-bold">{textMsg}</p>
+                <button
+                  onClick={() => setUserChoice(null)}
+                  className="border border-white bg-white font-semibold text-[20px] px-9 py-2 rounded-[5px] text-black  duration-200"
+                >
+                  PLAY AGAIN
+                </button>
+              </div>
+
+              {/* Opponent's Choice */}
+              <div className="flex flex-col items-center gap-3">
+                <h2 className="text-white mt-[50px] mb-[30px] font-bold">THE HOUSE PICKED</h2>
+                <div className="flex justify-center items-center bg-white border-[20px] border-red-600 h-[150px] w-[150px] rounded-[50%] hover:translate-y-[-5px] duration-200">
+                  {opponentChoice}
+                </div>
               </div>
             </div>
 
-            <p className=" text-white text-3xl font-bold">{textMsg}</p>
-
-            <button
-              onClick={() => {
-                setUserChoice(null);
-              }}
-              className=" border border-white border-transparent bg-blue-900 font-semibold text-2xl px-5 py-2 rounded-[5px] text-white hover:bg-blue-950 duration-200"
-            >
-              Restart
-            </button>
           </div>
         )}
       </div>
+
+      {/* Bottom Right Button */}
+      <button
+        onClick={() => alert("Bottom Right Button Clicked!")}
+        className="fixed bottom-5 border border-white right-5 bg-transparent text-white px-8 py-2 rounded-xl shadow-lg hover:bg-blue-700 duration-200"
+      >
+        Rules
+      </button>
     </main>
+
   );
 }
